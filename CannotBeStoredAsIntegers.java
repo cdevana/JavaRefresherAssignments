4d1bccd904ac5cd141ba14a5c870b144a696e263
cdevana <chaitanyadevana@yahoo.com>
2016-01-23 18:21:06 -0500

    Java Assignments 1 - 4

---
+++ CannotBeStoredAsIntegers.java
@@ -0,0 +1,11 @@
+package assignments;
+
+public class CannotBeStoredAsIntegers {
+	/*Not using this class for anything
+	 
+	 Example 1: 8.0 cannot be stored as int
+	 Example 2: 'A' cannot be stored as int
+	 
+	 */
+
+}
---
+++ SingleDeclarationVariables.java
@@ -0,0 +1,10 @@
+package assignments;
+
+public class SingleDeclarationVariables {
+
+	public static void main(String[] args) {
+		//Only b is initialized, but none of the variables are used yet.
+		int a, c, b = 4;
+	}
+
+}
